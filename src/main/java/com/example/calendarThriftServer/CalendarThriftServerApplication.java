684c2045d6322c5bb447c552fede1d53dd9d6f5a
ajay2728-dev <ajay.singh@capillarytech.com>
2025-02-10 16:15:55 +0530

    project setup

---
+++ src/main/java/com/example/calendarThriftServer/CalendarThriftServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.calendarThriftServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarThriftServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarThriftServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/calendarThriftServer/CalendarThriftServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calendarThriftServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarThriftServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
