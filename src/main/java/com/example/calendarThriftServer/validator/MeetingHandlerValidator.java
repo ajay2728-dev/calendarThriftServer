fc8c00c80af26c36719ca59e6ebab01632059b17
ajay2728-dev <ajay.singh@capillarytech.com>
2025-02-21 16:47:53 +0530

    checkAvailableRoom unit test added

--- src/main/java/com/example/calendarThriftServer/validator/MeetingHandlerValidator.java
+++ src/main/java/com/example/calendarThriftServer/validator/MeetingHandlerValidator.java
@@ -69,4 +69,6 @@ public class MeetingHandlerValidator {
         }
         return null;
     }
+
+
 }
--- src/test/java/com/example/calendarThriftServer/unitTest/MeetingHandlerValidatorTest.java
+++ src/test/java/com/example/calendarThriftServer/unitTest/MeetingHandlerValidatorTest.java
@@ -1,9 +1,11 @@
 package com.example.calendarThriftServer.unitTest;
 
 import com.example.calendarThriftServer.model.EmployeeModel;
+import com.example.calendarThriftServer.model.MeetingRoomModel;
 import com.example.calendarThriftServer.model.MeetingStatusModel;
 import com.example.calendarThriftServer.model.OfficeModel;
 import com.example.calendarThriftServer.repository.EmployeeRepo;
+import com.example.calendarThriftServer.repository.MeetingRoomRepo;
 import com.example.calendarThriftServer.repository.MeetingStatusRepo;
 import com.example.calendarThriftServer.validator.MeetingHandlerValidator;
 import com.example.thriftMeeting.IMeetingServiceDTO;
@@ -29,10 +31,14 @@ public class MeetingHandlerValidatorTest {
     @Mock
     private MeetingStatusRepo meetingStatusRepo;
 
+    @Mock
+    private MeetingRoomRepo meetingRoomRepo;
+
     @InjectMocks
     private MeetingHandlerValidator meetingHandlerValidator;
 
     private IMeetingServiceDTO meetingDTO;
+    private MeetingRoomModel meetingRoom;
     private EmployeeModel employee1;
     private OfficeModel office;
     private LocalDateTime start;
@@ -47,6 +53,8 @@ public class MeetingHandlerValidatorTest {
         meetingDTO.setStartTime("2025-02-18 10:00");
         meetingDTO.setEndTime("2025-02-18 11:00");
         meetingDTO.setEmployeeIDs(new ArrayList<>(Arrays.asList(1, 2)));
+
+        meetingRoom = new MeetingRoomModel(1,"Conference xyz",office,true);
 
         start = LocalDateTime.of(2025, 2, 21, 10, 0);
         end = LocalDateTime.of(2025, 2, 21, 11, 0);
@@ -77,6 +85,15 @@ public class MeetingHandlerValidatorTest {
                 meetingHandlerValidator.checkEmployeeMeetingConflict(meetingDTO, start, end));
 
         assertEquals("Employee with ID 1 is already booked during the selected time.", exception.getMessage());
+    }
+
+    @Test
+    void test_CheckAvailableRoom_Success(){
+        Mockito.when(employeeRepo.findById(Mockito.anyInt())).thenReturn(Optional.of(employee1));
+        Mockito.when(meetingRoomRepo.findAvailableMeetingRooms(Mockito.anyInt(), Mockito.any(), Mockito.any())).thenReturn(Arrays.asList(meetingRoom));
+        MeetingRoomModel availableRoom = meetingHandlerValidator.checkAvailableRoom(meetingDTO, start, end);
+
+        assertNotNull(availableRoom);
     }
 
 }
