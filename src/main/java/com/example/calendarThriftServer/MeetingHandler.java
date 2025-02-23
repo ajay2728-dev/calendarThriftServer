9e5516991b04c6f49e64244197e5019d0ae24d37
ajay2728-dev <ajay.singh@capillarytech.com>
2025-02-24 00:30:03 +0530

    async operation added

--- src/main/java/com/example/calendarThriftServer/MeetingHandler.java
+++ src/main/java/com/example/calendarThriftServer/MeetingHandler.java
@@ -140,4 +140,88 @@ public class MeetingHandler implements IMeetingService.Iface {
         return responseBody;
 
     }
+
+    // aysnc
+//    @Override
+//    @Transactional
+//    public IMeetingServiceDTO meetingSchedule(IMeetingServiceDTO meetingDTO) throws TException {
+//        return CompletableFuture.supplyAsync(() -> {
+//            try {
+//                return scheduleMeeting(meetingDTO);
+//            } catch (Exception ex) {
+//                throw new RuntimeException("Error scheduling meeting", ex);
+//            }
+//        }).join();
+//    }
+
+//    private IMeetingServiceDTO scheduleMeeting(IMeetingServiceDTO meetingDTO) throws TException {
+//        //          Parse startTime and endTime as LocalDateTime
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+//        LocalDateTime start = LocalDateTime.parse(meetingDTO.getStartTime(), formatter);
+//        LocalDateTime end = LocalDateTime.parse(meetingDTO.getEndTime(), formatter);
+//
+//        // checking employee timing conflict
+//        log.info("checking employee timing conflict ...");
+//        meetingHandlerValidator.checkEmployeeMeetingConflict(meetingDTO,start,end);
+//        log.info("checked employee timing conflict ...");
+//
+//        // check for valid room
+//        Optional<MeetingRoomModel> givenRoomOpt = meetingRoomRepo.findById(meetingDTO.getRoomId());
+//
+//        if(givenRoomOpt.isPresent()){
+//
+//            // check room is available between start and end time
+//            MeetingRoomModel validRoom = givenRoomOpt.get();
+//            MeetingModel newMeeting = new MeetingModel(meetingDTO.getDescription(),meetingDTO.getAgenda(),validRoom,start,end,true);
+//            MeetingModel saveMeeting = meetingRepo.save(newMeeting);
+//
+//            for (int employeeId : meetingDTO.getEmployeeIDs()) {
+//
+//                Optional<EmployeeModel> employeeOpt = employeeRepo.findById(employeeId);
+//                EmployeeMeetingStatusModel meetingStatus = new EmployeeMeetingStatusModel();
+//                meetingStatus.setMeeting(saveMeeting);
+//                meetingStatus.setMeetingStatus(false);
+//                meetingStatus.setEmployee(employeeOpt.get());
+//                employeeMeetingStatusRepo.save(meetingStatus);
+//
+//            }
+//
+//            IMeetingServiceDTO responseBody = new IMeetingServiceDTO(saveMeeting.getMeetingId(),saveMeeting.getDescription(),saveMeeting.getAgenda(),meetingDTO.getEmployeeIDs(),meetingDTO.getStartTime(),meetingDTO.getEndTime(),saveMeeting.getMeetingRoom().getRoomId());
+//            return responseBody;
+//        }
+//
+//
+//        // checking available room
+//        log.info("checking for available room ...");
+//        MeetingRoomModel roomAvailable = meetingHandlerValidator.checkAvailableRoom(meetingDTO,start,end);
+//        log.info("checked for available room ...");
+//
+//        if(roomAvailable==null){
+//            throw new MeetingException("No available meeting room for the selected time.",409);
+//        }
+//
+//        MeetingModel newMeeting = new MeetingModel(meetingDTO.getDescription(),meetingDTO.getAgenda(),roomAvailable,start,end,true);
+//
+//        MeetingModel saveMeeting = meetingRepo.save(newMeeting);
+//
+//        for (int employeeId : meetingDTO.getEmployeeIDs()) {
+//
+//            Optional<EmployeeModel> employeeOpt = employeeRepo.findById(employeeId);
+//            EmployeeMeetingStatusModel meetingStatus = new EmployeeMeetingStatusModel();
+//            meetingStatus.setMeeting(saveMeeting);
+//            meetingStatus.setMeetingStatus(false);
+//            meetingStatus.setEmployee(employeeOpt.get());
+//            try {
+//                employeeMeetingStatusRepo.save(meetingStatus);
+//            } catch (RuntimeException ex){
+//                throw new RuntimeException("Error occur while saving the employee meeting status..");
+//            }
+//
+//        }
+//
+//        IMeetingServiceDTO responseBody= new IMeetingServiceDTO(saveMeeting.getMeetingId(),saveMeeting.getDescription(),saveMeeting.getAgenda(),meetingDTO.getEmployeeIDs(),meetingDTO.getStartTime(),meetingDTO.getEndTime(),saveMeeting.getMeetingRoom().getRoomId());
+//        return responseBody;
+//    }
+
+
 }
